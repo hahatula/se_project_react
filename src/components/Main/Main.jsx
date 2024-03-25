e7ad2baca4dd736a4c8faeac2230860668222891
hahatula <olgagolubev.pro@gmail.com>
2024-03-25 18:18:13 +0200

    subscribed main to the context

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,20 @@
 import "./Main.css";
+import { useContext } from "react";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 import { defaultClothingItems } from "../../utils/constants";
+import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 
 function Main({ weatherData, handleItemClick }) {
+  const temperature = useContext(CurrentTemperatureUnitContext);
+  const temperatureUnit = temperature.currentTemperatureUnit;
+
   return (
     <main className="main">
       <WeatherCard weatherData={weatherData} />
       <section className="main__clothing">
         <p className="main__text">
-          Today is {weatherData.temperature.F} / You may want to wear:
+          Today is {weatherData.temperature[temperatureUnit]} / You may want to wear:
         </p>
         <ul className="main__items">
           {defaultClothingItems
