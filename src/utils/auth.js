3ad9d671d3588dc495795af00c18262dac229217
hahatula <olgagolubev.pro@gmail.com>
2024-05-28 18:23:26 +0300

    refactor auth requests

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,29 +1,25 @@
-import { BASE_URL } from "./constants";
+import { BASE_URL } from './constants';
+import { request, checkResponse } from './api';
 
 export const register = (name, avatar, email, password) => {
-    return fetch(`${BASE_URL}/signup`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, avatar, email, password }),
-    })
-      .then((res) => {
-        return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-      })
-  };
-  
-  // The authorize function accepts the necessary data as parameters.
-  export const authorize = (email, password) => {
-      return fetch(`${BASE_URL}/signin`, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      }).then((res) => {
-        return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-      });
-    };
+  return request(`${BASE_URL}/signup`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, avatar, email, password }),
+  });
+};
+
+// The authorize function accepts the necessary data as parameters.
+export const authorize = (email, password) => {
+  return request(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+};
